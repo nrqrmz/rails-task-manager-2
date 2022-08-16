671c36479a53869e5a563a0f6b95c6e18e73529a
Enrique Ramirez <nrqrmz@gmail.com>
2022-08-15 23:27:22 -0500

    Add routes/controller for #update

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -22,6 +22,13 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
 
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+
+    redirect_to task_path(@task)
+  end
+
   private
 
   def task_params
