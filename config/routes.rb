e55a19b16fee79cc6e25b1b9f55233ed71dac35e
Enrique Ramirez <nrqrmz@gmail.com>
2022-08-15 23:23:37 -0500

    Add routes/controller/view for #edit

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   get 'tasks/new', to: 'tasks#new', as: :new_task
   post 'tasks', to: 'tasks#create'
   get 'tasks/:id', to: 'tasks#show', as: :task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
 end
